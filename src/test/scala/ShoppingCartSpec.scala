3719eee4aebe919f44da60dd0e77a17ff69f5351
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2024-05-26 16:26:55 +0100

    calculate subtotal from items in shopping cart

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -2,9 +2,24 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 
 case class Price(value: BigDecimal)
+object Price {
+  def apply(value: String): Price = Price(BigDecimal(value))
+}
 
-case class ShoppingCart() {
-  def subtotal: Price = Price(BigDecimal(0))
+case class Item(name: String)
+
+case class PricedItem(item: Item, price: Price)
+
+case class Quantity(value: BigDecimal)
+
+case class ShoppingCart(items: Map[PricedItem, Quantity]) {
+  def subtotal: Price = Price(items.map {
+    case (item, quantity) => item.price.value * quantity.value
+  }.sum)
+}
+object ShoppingCart {
+  def apply(): ShoppingCart = ShoppingCart(Map.empty[PricedItem, Quantity])
+  def apply(items: (PricedItem, Quantity)*): ShoppingCart = ShoppingCart(Map(items: _*))
 }
 
 class ShoppingCartSpec extends AnyFreeSpec with Matchers {
@@ -12,7 +27,12 @@ class ShoppingCartSpec extends AnyFreeSpec with Matchers {
   "ShoppingCart" - {
     "subtotal" - {
       "with no items, should be 0" in {
-        ShoppingCart().subtotal mustBe Price(BigDecimal(0))
+        ShoppingCart().subtotal mustBe Price(0)
+      }
+      "with one item, should be equal to it's price multiplied by it's quantity" in {
+        ShoppingCart(
+          PricedItem(Item("cornflakes"), Price("5.00")) -> Quantity(5)
+        ).subtotal mustBe Price("25.00")
       }
     }
   }
