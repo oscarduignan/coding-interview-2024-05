88e2e3c570295bbb4612e32e863b7d3ae18033d5
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2024-05-26 17:19:18 +0100

    allow adding new items to cart and round prices correctly

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -1,11 +1,12 @@
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 
-case class Price(value: BigDecimal) {
+case class Price private (value: BigDecimal) {
     def taxPayable(implicit taxRate: TaxRate): Price = Price(value * taxRate.multiplier)
 }
 object Price {
   def apply(value: String): Price = Price(BigDecimal(value))
+  def apply(value: BigDecimal): Price = new Price(value.setScale(2, BigDecimal.RoundingMode.UP))
 }
 
 case class TaxRate(multiplier: BigDecimal)
@@ -22,6 +23,13 @@ case class ShoppingCart(items: Map[PricedItem, Quantity]) {
     case (item, quantity) => item.price.value * quantity.value
   }.sum)
   def taxPayable(implicit taxRate: TaxRate): Price = subtotal.taxPayable
+  def totalPayable(implicit taxRate: TaxRate): Price = Price(subtotal.value + subtotal.taxPayable.value)
+  def add(item: PricedItem, quantity: Quantity): ShoppingCart =
+    this.copy(items =
+      items.updated(
+        item,
+        Quantity(items.get(item).map(_.value).getOrElse(BigDecimal(0)) + quantity.value)
+      ))
 }
 object ShoppingCart {
   def apply(): ShoppingCart = ShoppingCart(Map.empty[PricedItem, Quantity])
@@ -68,6 +76,38 @@ class ShoppingCartSpec extends AnyFreeSpec with Matchers {
           PricedItem(Item("frosties"), Price("2.50")) -> Quantity(1),
           PricedItem(Item("weetabix"), Price("2.50")) -> Quantity(3),
         ).taxPayable mustBe Price("7.00")
+      }
+    }
+    "totalPayable" - {
+      "with no items, should be 0" in {
+        ShoppingCart().totalPayable mustBe Price(0)
+      }
+      "with multiple items, should be equal to their subtotal multiplied by the tax rate" in {
+        ShoppingCart(
+          PricedItem(Item("cornflakes"), Price("5.00")) -> Quantity(5),
+          PricedItem(Item("frosties"), Price("2.50")) -> Quantity(1),
+          PricedItem(Item("weetabix"), Price("2.50")) -> Quantity(3),
+        ).totalPayable mustBe Price("42.00")
+      }
+    }
+    "add" - {
+      "when the same priced item exists, should increase the quantity" in {
+        ShoppingCart(
+          PricedItem(Item("cornflakes"), Price("2.52")) -> Quantity(1)
+        )
+          .add(PricedItem(Item("cornflakes"), Price("2.52")), Quantity(1))
+          .subtotal mustBe Price("5.04")
+      }
+      "when it doesn't already exist, should add it to the item list" in {
+        val twelveAndAHalfPercentTaxRate: TaxRate = TaxRate(0.125)
+
+        val exampleFromReadme = ShoppingCart(
+          PricedItem(Item("cornflakes"), Price("2.52")) -> Quantity(2)
+        ).add(PricedItem(Item("weetabix"), Price("9.98")), Quantity(1))
+
+        exampleFromReadme.subtotal mustBe Price("15.02")
+        exampleFromReadme.taxPayable(twelveAndAHalfPercentTaxRate) mustBe Price("1.88")
+        exampleFromReadme.totalPayable(twelveAndAHalfPercentTaxRate) mustBe Price("16.90")
       }
     }
   }
